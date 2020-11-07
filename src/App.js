3d0cf4e1ca61693fbfbf8a4a909110bf479e8030
riky1979 <riky1979@naver.com>
2020-11-07 22:50:03 +0900

    Create Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <h2>이서준의 홈페이지 입니다!!</h2>
+    </div>
+  );
+}
+
+export default App;
