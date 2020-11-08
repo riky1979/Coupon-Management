e9c5ddf3a34853e0a4f57f3d53c74fd8c1aca984
riky1979 <riky1979@naver.com>
2020-11-09 00:12:26 +0900

    Create Customer DB Table & Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,37 +7,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/', (req, res) => {
     res.send({message: 'Hello Home!'});
 });
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            "image" : "https://placeimg.com/64/64/any",
-            'name' : '홍길동',
-            'birthday' : '17791191',
-            'gender' : '남자',
-            'job' : '대학생',
-            },
-            {
-              'id' : 2,
-              "image" : "https://placeimg.com/64/64/any",
-              'name' : '이순신',
-              'birthday' : '12330615',
-              'gender' : '남자',
-              'job' : '대학생',
-            },
-            {
-                'id' : 3,
-                "image" : "https://placeimg.com/64/64/any",
-                'name' : '세종대왕',
-                'birthday' : '14340223',
-                'gender' : '남자',
-                'job' : '대학생',
-            }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+          res.send(rows);
+      }
+    );
 });
 
 
