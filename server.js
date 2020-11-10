f66824f1052a49df8d2c6a362a2355fa0e52efc6
riky1979 <riky1979@naver.com>
2020-11-11 01:40:20 +0900

    Add Tutorial Souce Files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
@@ -21,41 +21,49 @@ const connection = mysql.createConnection({
 connection.connect();
 
 const multer = require('multer');
-const { connect } = require('http2');
-const upload = multer({dest: './upload'});
-
-app.get('/', (req, res) => {
-    res.send({message: 'Hello Home!'});
-});
+const upload = multer({dest: './upload'})
 
 app.get('/api/customers', (req, res) => {
     connection.query(
-      "SELECT * FROM CUSTOMER",
+      "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
       (err, rows, fields) => {
           res.send(rows);
-      }
+      }  
     );
 });
 
-app.use('/image', express.static('./upload'))
+app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), null, 0)';
     let image = '/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
     let gender = req.body.gender;
     let job = req.body.job;
     let params = [image, name, birthday, gender, job];
-    console.log(params);
+    connection.query(sql, params, 
+        (err, rows, fields) => {
+            res.send(rows);
+            console.log(rows);
+            console.log(err);
+        }
+    );
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+    let sql = 'UPDATE CUSTOMER SET isDeleted = 1, deletedDate = now() WHERE id = ?';
+    let params = [req.params.id];
     connection.query(sql, params,
         (err, rows, fields) => {
             res.send(rows);
-            console.log(err);
-            console.log(rows);
         }
-    );
-})
+    )
+});
 
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello Home!'});
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
